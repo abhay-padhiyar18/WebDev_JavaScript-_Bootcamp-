e4dac7a936e96d96ec826a58cbdb65f7bfbadc58
ajpadhiyar18 <abhaypadhiyar01@gmail.com>
2025-09-27 18:02:35 +0530

    day02

---
+++ Day 02/first.js
@@ -0,0 +1,24 @@
+// 1. Using let (can be changed later)
+let age = 21;
+console.log("Age:", age); // Output: Age: 21
+
+// Changing the value
+age = 22;
+console.log("Updated Age:", age); // Output: Updated Age: 22
+
+// 2. Using const (cannot be changed)
+const pi = 3.1416;
+console.log("Value of PI:", pi);
+
+// 3. Using var (old way, avoid in modern JS)
+var name = "Abhay";
+console.log("Name:", name);
+
+// 4. Different data types in variables
+let isStudent = true;            // Boolean
+let marks = 85.5;                // Number
+let subject = "JavaScript";      // String
+let skills = ["HTML", "CSS"];    // Array
+let details = { city: "Vadodara", course: "MCA" }; // Object
+
+console.log(isStudent, marks, subject, skills, details);
